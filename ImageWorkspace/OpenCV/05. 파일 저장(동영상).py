810812d7bd7909f06c2b182eec78766bbc911e74
DarkKaiser <darkkaiser@gmail.com>
2023-10-08 19:33:05 +0900

    no message

---
+++ ImageWorkspace/OpenCV/05. 파일 저장(동영상).py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture("video.mp4")
+
+# 코덱 정의
+fourcc = cv2.VideoWriter_fourcc(*"DIVX")
+
+width = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+out = cv2.VideoWriter("output.avi", fourcc, fps, (width, height))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    if ret == False:
+        break
+    
+    out.write(frame)    # 영상 데이터만 저장(소리는 저장되지 않는다)
+    
+out.release()
+cap.release()
